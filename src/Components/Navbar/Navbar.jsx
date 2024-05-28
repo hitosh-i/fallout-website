0d50af781cd24e9921321723c1a79a81c3ef9480
hitoshi-i <abdoulmbarry456@gmail.com>
2024-05-28 03:50:32 -0400

    adding github pages stuff

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,11 @@ export const Navbar = () => {
         <li>Home</li>
         <li>Explore</li>
         <li>About</li>
-        <li className='nav-contact'>Steam Page</li>
+        <li className='nav-contact'>
+          <a href="https://store.steampowered.com/app/22380/Fallout_New_Vegas/" target="_blank" rel="noopener noreferrer">
+            Steam Page
+          </a>
+        </li>
       </ul>
     </div>
   );
